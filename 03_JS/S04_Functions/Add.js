b6f037e9931b990589abab6b4be549f42dce7f47
Kabir <sagithedeveloper@gmail.com>
2024-12-07 16:29:11 +0530

    Added the code

---
+++ 03_JS/S04_Functions/Add.js
@@ -0,0 +1,36 @@
+var x = 10;
+
+var y = 20;
+
+var result = x + y;
+
+console.log(result);
+
+var x = 10;
+
+var y = 20;
+
+var result = x + y;
+
+console.log(result);
+var x = 10;
+
+var y = 20;
+
+var result = x + y;
+
+console.log(result);
+var x = 10;
+
+var y = 20;
+
+var result = x + y;
+
+console.log(result);
+var x = 10;
+
+var y = 20;
+
+var result = x + y;
+
+console.log(result);
---
+++ 03_JS/S04_Functions/fnAdd.js
@@ -0,0 +1,15 @@
+function fnAdd() {
+  var x = 10;
+
+  var y = 20;
+
+  var result = x + y;
+
+  console.log(result);
+}
+
+fnAdd(); //function call
+fnAdd();
+fnAdd();
+fnAdd();
+fnAdd();
