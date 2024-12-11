9a478ae0fa1ba87268be596beaee729de99ed7ee
Kabir <sagithedeveloper@gmail.com>
2024-12-11 06:38:09 +0530

    Added the code

---
+++ 03_JS/S05_functions/Task1.js
@@ -0,0 +1,13 @@
+// var x = function f1() {
+//   console.log("Working");
+// };
+// console.log(x);
+
+function f1() {
+  console.log("working");
+  return true;
+  console.log("hello");
+}
+
+var x = f1();
+console.log(x);
---
+++ 03_JS/S05_functions/function.js
@@ -0,0 +1,12 @@
+function f1(a, b) {
+  var x = 20;
+  var y = 30;
+
+  var result = x + y;
+
+  console.log(a + b);
+}
+
+f1(20, 30); //20+30
+f1(40, 50); //10+30
+f1(100, 100); //100+100
