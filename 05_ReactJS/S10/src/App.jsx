e567662df86735fc71892164f759ff7a3380cc1d
Kabir <sagithedeveloper@gmail.com>
2025-03-03 15:40:39 +0530

    Added the code

---
+++ 05_ReactJS/S10/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Nav from "./components/nav/Nav";
+import StateConcept from "./components/state-concept/StateConcept";
+function App() {
+  return (
+    <div>
+      <Nav />
+      <StateConcept />
+    </div>
+  );
+}
+
+export default App;
