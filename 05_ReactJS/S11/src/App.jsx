2209b621c79b8020b76b5e3a70c4f0edeb2e8806
Kabir <sagithedeveloper@gmail.com>
2025-03-07 15:11:04 +0530

    added the code

---
+++ 05_ReactJS/S11/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Nav from "./components/nav/Nav";
+import StateCom from "./components/state-concept/StateCom";
+import StateCom2 from "./components/state-concept/StateComp2";
+import User from "./components/state-concept/User";
+function App() {
+  return (
+    <div>
+      <Nav />
+      <User />
+    </div>
+  );
+}
+
+export default App;
